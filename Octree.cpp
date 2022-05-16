f623cfa5f31f8feaaad8a82e3f96d26819880115
rbugge <ryan.bugge@tuta.io>
2022-05-16 04:24:43 -0400

    Collect function rewritten

--- Octree.cpp
+++ Octree.cpp
@@ -2,13 +2,11 @@
 
 #include "VertexIDHashing.hpp"
 
-
 using namespace OctreeDefinition;
 using namespace VertexIDHashingDefinition;
 
 OctreeDefinition::Octree::Octree()
 {
-
 }
 
 OctreeDefinition::Octree::~Octree()
@@ -17,133 +15,218 @@ OctreeDefinition::Octree::~Octree()
 
 void OctreeDefinition::Octree::buildOctree()
 {
-	TriangleOctantKeyPairList::loadTriangleOctantKeyPairList();
+    TriangleOctantKeyPairList::loadTriangleOctantKeyPairList();
 }
 
 void OctreeDefinition::Octree::octreePrintStats()
 {
-	say "Octree Debug" done;
-	say "\tPoint Limit:" spc octreeLimit done;
-	say "\tDepth Limit:" spc octreeDepthLimit done;
-	say "\tDepth:" spc octreeDepth done;
-	// say "\tPoints: " << points.size() done; - need to reimplement
-	say "\tEdges:" spc edgesTotal() done;
-	say "\tTriangles:" spc totalTriangles() done;
-	say "\tOctants:" spc octantsTotal() done;
-	say "\tLeaves:" spc leaves.size() done;
+    say "Octree Debug" done;
+    say "\tPoint Limit:" spc octreeLimit done;
+    say "\tDepth Limit:" spc octreeDepthLimit done;
+    say "\tDepth:" spc octreeDepth done;
+    // say "\tPoints: " << points.size() done; - need to reimplement
+    say "\tEdges:" spc edgesTotal() done;
+    say "\tTriangles:" spc totalTriangles() done;
+    say "\tOctants:" spc octantsTotal() done;
+    say "\tLeaves:" spc leaves.size() done;
 
-	say "\tCenter:" spc to_string(octants[0].octantCenter) done;
-	say "\tHalfsize:" spc octants[0].octantHalfSize done;
-	say "\tTriangles in Root:" spc octants[0].octantTotalTriangles() done;
+    say "\tCenter:" spc to_string(octants[0].octantCenter) done;
+    say "\tHalfsize:" spc octants[0].octantHalfSize done;
+    say "\tTriangles in Root:" spc octants[0].octantTotalTriangles() done;
 
-	int count = 0;
-	int owt = 0;
-	int tip = 0;
-	foreach(o, octants)
-	{
-		if (o.octantState)
-		{
-			count++;
-		}
-		
-		if (o.octantTotalTriangles()) 
-		{
-			owt++;
-			if (o.octantState == OctantState::OctantNotEmptyInternal) // this really needs to be enumerated to say what is going on.
-			{
-				tip++;
-			}
-				
-		}
-	}
+    int count = 0;
+    int owt = 0;
+    int tip = 0;
+    foreach (o, octants)
+    {
+        if (o.octantState)
+        {
+            count++;
+        }
 
-	say "\tActive Octants:" spc count done;
-	say "\tOctants with Triangles:" spc owt done;
-	say "\tPsuedo-Leaves w/ Traingles:" spc tip done;
+        if (o.octantTotalTriangles())
+        {
+            owt++;
+            if (o.octantState == OctantState::OctantNotEmptyInternal) // this really needs to be enumerated to say what is going on.
+            {
+                tip++;
+            }
+        }
+    }
 
-	// Not implementing for now.
-	/*
-	say "Octree Mesh:" done;
+    say "\tActive Octants:" spc count done;
+    say "\tOctants with Triangles:" spc owt done;
+    say "\tPsuedo-Leaves w/ Traingles:" spc tip done;
+
+    // Not implementing for now.
+    /*
+    say "Octree Mesh:" done;
     say "\tVertices: " << octreeVertices.size() done;
     say "\tIndices: " << octreeIndices.size() done;
-	*/
-
-
-
+    */
 }
 
 bool OctreeDefinition::Octree::octantPointsInBound(RIndexTriangle tri, OctantIndex octantID)
 {
-	OctantReference oct = octants[octantID]; // retrieve the current processed octant
+    OctantReference oct = octants[octantID]; // retrieve the current processed octant
 
-	for (int i = 0; i < 3; i++)
-	{
-		rv3 pos = vertices[tri[i]].position;
-		// what is this supposed to be doing?
-		if (oct.octantHalfSizeCenterComparison(pos))
-		{
-			return false;
-		}
-	}
-	return true;
+    for (int i = 0; i < 3; i++)
+    {
+        rv3 pos = vertices[tri[i]].position;
+        // what is this supposed to be doing?
+        if (oct.octantHalfSizeCenterComparison(pos))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 const int OctreeDefinition::Octree::octantsTotal()
 {
-	return octants.size();
+    return octants.size();
 }
 
 const int OctreeDefinition::Octree::octantsLeavesTotal()
 {
-	return leaves.size();
+    return leaves.size();
 }
 
-auto OctreeDefinition::Octree::collect(OctreeCollision& collision, double range)
+// auto OctreeDefinition::Octree::collect(OctreeCollision &collision, double range)
+// {
+//     // this code is not properly structured
+
+//     KeyList results; // list of keys returned from collecting method
+
+//     std::unordered_set<KeyData, point_hash, point_equal> unchecked;     // needs commenting
+//     std::unordered_set<KeyData, point_hash, point_equal> tempUnchecked; // needs commenting
+//     std::unordered_set<KeyData, point_hash, point_equal> checked;       // needs commenting - what is checked
+
+//     for (int i = 0; i < 3; i++)
+//     {
+//         unchecked.insert(triangles[collision.triangleID][i]);
+//     }
+
+//     while (!unchecked.empty())
+//     {
+//         tempUnchecked = unchecked; // this seems extremely performance inefficient to copy over containers like this.
+
+//         // for each element in unchecked
+//         foreach (uncheckedElement, unchecked)
+//         {
+//             // if distance between the collision and unchecked element is less than or equal to the range
+//             if (distance(vertices[uncheckedElement].position, collision.position) <= range)
+//             {
+//                 // load the unchecked element into results
+//                 results.emplace_back(uncheckedElement);
+//                 // now check the elements edges
+//                 // Add each point that is connected by an edge to this point
+//                 foreach (vertexEdge, edges[uncheckedElement].vertexEdges)
+//                 {
+//                     // Left OFF Here - Need to figure out what is going on with this logic. - I shouldn't try and remake the structure but all of these additional helper structures are a mess.
+//                 }
+//             }
+//         }
+//     }
+
+//     return;
+// }
+
+
+// Rewritten to avoid copying the entire unchecked set.
+// Collect all vertices within a given range of the collision
+auto OctreeDefinition::Octree::collect(OctreeCollision &collision, double range)
 {
-	// this code is not properly structured
+    KeyList results; // list of keys returned from collecting method
 
-	KeyList results; // list of keys returned from collecting method
+    std::unordered_set<KeyData, point_hash, point_equal> unchecked; // List of vertices to check if they're within range
+    std::unordered_set<KeyData, point_hash, point_equal> checked;   // Every vertex that has been checked
 
-	std::unordered_set<KeyData, point_hash, point_equal> unchecked; // needs commenting
-	std::unordered_set<KeyData, point_hash, point_equal> tempUnchecked;  // needs commenting
-	std::unordered_set<KeyData, point_hash, point_equal> checked;  // needs commenting - what is checked
+    // Prime the unchecked set with the triangle vertices
+    unchecked.insert(triangles[collision.triangleID][0]);
+    unchecked.insert(triangles[collision.triangleID][1]);
+    unchecked.insert(triangles[collision.triangleID][2]);
 
-	for (int i = 0; i < 3; i++)
-	{
-		unchecked.insert(triangles[collision.triangleID][i]);
-	}
+    // While there are still unchecked vertices
+    while (!unchecked.empty())
+    {
+        // Copy and move the first element of the unchecked set to checked
+        KeyData element = *unchecked.begin();
+        unchecked.erase(unchecked.begin());
+        checked.insert(element);
 
-	while (!unchecked.empty())
-	{
-		tempUnchecked = unchecked; // this seems extremely performance inefficient to copy over containers like this.
+        // Check distance between the collision and the element
+        if (distance(vertices[element].position, collision.position) <= range)
+        {
+            results.emplace_back(element); // Add the element to the results list
 
-		// for each element in unchecked
-		foreach(uncheckedElement, unchecked)
-		{
-			// if distance between the collision and unchecked element is less than or equal to the range
-			if (distance(vertices[uncheckedElement].position, collision.position) <= range)
-			{
-				// load the unchecked element into results
-				results.emplace_back(uncheckedElement);
-				// now check the elements edges
-				// Add each point that is connected by an edge to this point
-				foreach(vertexEdge, edges[uncheckedElement].vertexEdges)
-				{
-					// Left OFF Here - Need to figure out what is going on with this logic. - I shouldn't try and remake the structure but all of these additional helper structures are a mess.
-				}
-			}
-		}
+            // Add each vertex that is connected by an edge to this vertex
+            // and that has not already been checked
+            // Using unordered set allows us to insert into unchecked whether or not it already exists.
+            foreach (vertex, edges[element].vertexEdges)
+            {
+                if (!checked.contains(vertex))
+                {
+                    unchecked.insert(vertex);
+                }
+            }
+        }
+    }
 
-	}
-
-	return;
+    return results;
 }
 
+// vector<Point> Octree::collect(Collision &collision, double range)
+// {
+//     vector<Point> points;
+//     std::unordered_set<Point, point_hash, point_equal> unchecked; // needs commenting
+//     std::unordered_set<Point, point_hash, point_equal> tempUnchecked;  // needs commenting
+//     std::unordered_set<Point, point_hash, point_equal> checked;  // needs commenting - what is checked
 
+//     // Insert intersected triangle's points into unchecked
+//     foreach (p, triangles[collision.triangle].points)
+//     {
+//         unchecked.insert(p);
+//     }
 
+//     // While unchecked is not empty
+//     while (!unchecked.empty())
+//     {
+//         tempUnchecked = unchecked;
+//         // For each unchecked point
+//         foreach (p, unchecked)
+//         {
 
+//             // If distance to point is within range
+//             if (distance(getVertexPosition(p.index), collision.pos) <= range)
+//             {
+//                 points.emplace_back(p);
+//                 // Add each point that is connected by an edge to this point
+//                 foreach (edge, p.edges)
+//                 {
+// 					// oddly programmed - what is occuring here logically?
+//                     GEdge temp = edges[edge];
+//                     Point otherPoint = temp.getOtherPoint(p.index);
 
+//                     // Check if point was checked
+//                     if (!checked.contains(otherPoint))
+//                     {
+//                         // If not add to tempUnchecked
+//                         tempUnchecked.insert(otherPoint);
+//                     }
+//                 }
+//             }
 
+//             // Add to checked and remove from unchecked
+//             checked.insert(p);
+//             tempUnchecked.erase(p);
+//         }
+
+//         unchecked = tempUnchecked;
+//     }
+
+//     return points;
+// }
 
 /*
 #include "Octree.hpp"
@@ -151,7 +234,7 @@ auto OctreeDefinition::Octree::collect(OctreeCollision& collision, double range)
 Octree::Octree(vector<v3> &verts, vector<int> &indices)
 {
 
-    
+
     points.reserve(verts.size());
     triangles = vector<Triangle>(indices.size() / 3);
     edges.reserve((int)ceil(indices.size() / 2));
@@ -291,9 +374,9 @@ vector<Point> Octree::collect(Collision &collision, double range)
                 // Add each point that is connected by an edge to this point
                 foreach (edge, p.edges)
                 {
-					// oddly programmed - what is occuring here logically?
+                    // oddly programmed - what is occuring here logically?
                     GEdge temp = edges[edge];
-                    Point otherPoint = temp.getOtherPoint(p.index); 
+                    Point otherPoint = temp.getOtherPoint(p.index);
 
                     // Check if point was checked
                     if (!checked.contains(otherPoint))
@@ -667,5 +750,3 @@ void Octree::update(vector<int> tris)
     }
 }
 */
-
-
