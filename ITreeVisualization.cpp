a7bb63e3d07a63c87c3fc586610bf85c411415b5
DamItGraphics <97328279+DamItGraphics@users.noreply.github.com>
2022-05-15 22:52:58 -0400

    Deez some more changes

--- ITreeVisualization.cpp
+++ ITreeVisualization.cpp
@@ -18,10 +18,16 @@ void ITreeVisualizationDefinition::ITreeVisualization::generateITreeVisualizatio
 	{
 		inode.generateVisualVertices(itreeDrawContainer.itreeDrawVertex, itreeDrawContainer.itreeDrawIndices, tracker);
 	}
-
+	forall(vert, itreeDrawContainer.itreeDrawVertex)
+	{
+		vert.loadColorAs255(255, 0, 0);
+		say "V:" spc vert.position.x spc vert.position.y spc vert.position.z done;
+	}
 	itreeDrawContainer.bindContainer();
 
 	say "ITree Vertice Count" spc itreeDrawContainer.itreeDrawVertex.size() done;
 	say "Itree Indice Count" spc itreeDrawContainer.itreeDrawIndices.size() done;
+
+
 	
 }
