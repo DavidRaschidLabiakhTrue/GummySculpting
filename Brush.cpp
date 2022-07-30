9b989bca4a74439c3333d1b1f33902cba87810ce
DamItGraphics <97328279+DamItGraphics@users.noreply.github.com>
2022-07-30 14:31:36 -0400

    BrushBrush bonus

--- Brush.cpp
+++ Brush.cpp
@@ -2,34 +2,37 @@
 
 void Sculpting::Brush::applyBrush(MeshReference cMesh, SculptPayloadReference payload)
 {
-    foreach (vertex, cMesh.currentVertices)
-    {
-        const float dist = distance(vertex.second.position, payload.hit);
-        const float curve = ((glm::cos((dist / payload.radius) * pi<float>()) + 1.0f) * 0.5f);
-        const float ang = angle(vertex.second.normal, payload.hitNorm);
-        const float angLim = glm::radians(60.0f);
-        const float scale = 0.05f;
+	const auto hit = payload.hit;
+	const auto angLim = glm::radians(60.0f);
+	const float scale = 0.05;
 
-        if (ang < angLim)
-        {
-            float modifier = ((glm::cos((ang / angLim) * pi<float>()) + 1.0f) * 0.5f);
+	const auto hitNorm = payload.hitNorm;
+	const float radius = payload.radius;
 
-            vertex.second.position += payload.polarity *
-                                      payload.radius *
-                                      scale *
-                                      curve *
-                                      normalize(modifier * vertex.second.normal + (1 - modifier) * payload.hitNorm);
-            //   vertex.second.normal;
-            //   normalize((0.75f * vertex.second.normal + 0.25f * payload.direction));
-        }
-        else
-        {
-            vertex.second.position += payload.polarity *
-                                      payload.radius *
-                                      scale *
-                                      curve *
-                                      payload.hitNorm;
-        }
-    }
+
+	const float piVal = pi<float>();
+
+	const float brushConst = payload.polarity * payload.radius * scale;
+
+	auto end = cMesh.currentVertices.end();
+	// parallize
+	#pragma loop(hint_parallel(8))
+	for (auto vertex = cMesh.currentVertices.begin(); vertex != end; ++vertex)
+	{
+		const float curve = ((glm::cos((distance((*vertex).second.position, payload.hit) / radius) * piVal) + 1.0f) * 0.5f);
+		const float ang = angle((*vertex).second.normal, hitNorm);
+
+		if (ang < angLim)
+		{
+			const float modifier = ((glm::cos((ang / angLim) * piVal) + 1.0f) * 0.5f);
+
+			(*vertex).second.position += brushConst * curve * normalize(modifier * (*vertex).second.normal + (1 - modifier) * hitNorm);
+		}
+		else
+		{
+			(*vertex).second.position += brushConst * curve * hitNorm;
+		}
+	}
+
     Algos::applyCurrentVerticesToMesh(cMesh);
 }
